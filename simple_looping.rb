05dc065b98beadafe9d8a212625563dbe30f0037
Christian Lim <christiananthonylim1@gmail.com>
2019-05-24 17:25:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,7 @@ def loop_iterator(number_of_times)
     phrase = "Welcome to Flatiron School's Web Development Course!"
     puts phrase
     counter = 0
-    counter ++
+    counter += 1
     break if counter == number_of_times
   end
 end
